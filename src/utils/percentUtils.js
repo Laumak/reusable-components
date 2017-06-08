4f911934efc56e58916e9d4839ea9450922f5a2f
Mäkinen Lauri <lauri.makinen@digia.com>
2017-06-08 14:22:43 +0300

    Unit test

---
+++ src/utils/percentUtils.js
@@ -0,0 +1,6 @@
+// Returns the width of a percent of total width, as an integer
+const getWidthAsPercentOfTotalWidth = (percent, totalWidth) => {
+  return parseInt(totalWidth * (percent / 100), 10)
+}
+
+export { getWidthAsPercentOfTotalWidth }
