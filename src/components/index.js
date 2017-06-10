297daa8b603d18250bf596a0caea1c10c07da998
Lauri Mäkinen <lauri@mits.fi>
2017-06-10 16:12:21 +0300

    Named exports

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as EyeIcon } from "./EyeIcon"
+export { default as ProgressBar } from "./ProgressBar"
+export { default as Label } from "./Label"
+export { default as TextInput } from "./TextInput"
+export { default as PasswordInput } from "./PasswordInput"
