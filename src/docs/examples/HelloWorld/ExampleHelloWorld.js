a4aae8fa8acd44423eaec4c7a09dc642a70d2e85
Lauri Mäkinen <lauri@mits.fi>
2017-06-10 15:51:37 +0300

    Get rid of unneeded component(s)

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import HelloWorld from "ps-react/HelloWorld"
-
-/** Custom message */
-const ExampleHelloWorld = props =>
-    <HelloWorld message="Testing props" />
-
-export default ExampleHelloWorld
